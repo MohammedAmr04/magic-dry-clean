d975ea790abfb95f745dff693e6ab13e74051186
Omar Ahmed <omar.ahmed9806@gmail.com>
2025-08-31 18:35:13 +0300

    setup

---
+++ client/src/services/constants/COLORS.ts
@@ -0,0 +1,17 @@
+// all colors must be in hsl format
+export const MAIN_COLORS = {
+  light: {
+    primary: '#1591fa',
+    background: '#FAFAFA',
+    text: '#0F0F0F',
+    card: '#ffffff',
+    border: '#E8E8E9',
+    input: '#FFFFFF',
+    danger: '#B81428',
+    'danger-content': '#FFD8E4',
+    success: '#136741',
+    warning: '#FFB200',
+    info: '#05B2DC',
+  },
+  dark: {}
+}
